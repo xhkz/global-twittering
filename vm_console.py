964fb101c09a3c84455a3e672111219dd80fbf6a
pOOOOOr <au.huangxin@gmail.com>
2015-04-19 18:25:13 +1000

    Script  for deploying virtual machines. TODO: configure environments.

---
+++ vm_console.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import argparse
+import time
+from boto.ec2.connection import EC2Connection
+from boto.ec2.regioninfo import RegionInfo
+
+
+# Team 8
+# TODO change and move to config file
+EC2_ACCESS_KEY = '38aa6ac0953443f5831398a4cc8757a0'
+EC2_SECRET_KEY = 'c1385348cf7e4b01b82601c2c2f7d61f'
+DEFAULT_IMAGE_ID = 'ami-000022b3'
+DEFAULT_KEY_PAIR = 'xhuang'
+DEFAULT_PLACE = 'melbourne-np'
+
+conn = EC2Connection(aws_access_key_id=EC2_ACCESS_KEY,
+                     aws_secret_access_key=EC2_SECRET_KEY,
+                     is_secure=True,
+                     region=RegionInfo(name="NeCTAR", endpoint="nova.rc.nectar.org.au"),
+                     validate_certs=False,
+                     port=8773,
+                     path="/services/Cloud")
+
+
+def create_instances(count=1):
+    # ami-000022b3 NeCTAR Ubuntu 14.04 (Trusty) amd64
+    instance_list = []
+    for c in range(count):
+        instance_list.append(
+            conn.run_instances('ami-000022b3',
+                               key_name=DEFAULT_KEY_PAIR,
+                               instance_type='m1.small',
+                               security_groups=['default'],
+                               placement=DEFAULT_PLACE)
+        )
+
+    return map(lambda x: x.instances[0], instance_list)
+
+
+def get_all_instance_ids():
+    return map(lambda x: x.instances[0].id, conn.get_all_instances())
+
+
+def get_instance(instance_id):
+    return conn.get_all_instances(instance_id)[0].instances[0]
+
+
+def check_status(instance_id):
+    print 'checking status of [%s]...' % instance_id
+    return get_instance(instance_id).state
+
+
+def wait_status(instance_id, status, max_loop=20):
+    loop = 0
+
+    while loop < max_loop:
+        time.sleep(15)
+        if check_status(instance_id) == status:
+            return True
+        loop += 1
+
+    return False
+
+
+def terminate_instance(instance_id):
+    return get_instance(instance_id).terminate()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', type=int, default=1, help='instance count')
+    parser.add_argument('-o', type=str, help='start or terminate')
+    args = parser.parse_args()
+
+    if not args.o:
+        parser.print_help()
+        exit(1)
+
+    if args.o.lower() == 'start':
+        for ins in create_instances(args.n):
+            if wait_status(ins.id, 'running'):
+                instance = get_instance(ins.id)
+                print instance.id, instance.ip_address, instance.state
+            else:
+                print 'create %s failed.' % ins.id
+    elif args.o.lower() == 'terminate':
+        # TODO
+        pass
+    else:
+        parser.print_help()
+
+
