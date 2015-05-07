acad8ea24bd0e2e7f91d90008a8608d90adf6c36
pOOOOOr <au.huangxin@gmail.com>
2015-05-07 17:56:17 +1000

    some basic charts and data

--- web/app/views/routes.py
+++ web/app/views/routes.py
@@ -1,36 +1,83 @@
 __author__ = 'Xin Huang'
-import os
 
-from flask import render_template, jsonify, send_from_directory
+from flask import render_template, jsonify
 
 from app import app
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
-                               mimetype='image/vnd.microsoft.icon')
-
-
 @app.route('/')
-def home():
-    return render_template('home.html')
+def pie():
+    return render_template('pie.html')
 
 
-@app.route('/data')
-def data():
+@app.route('/pie_data')
+def pie_data():
     response = {
-        "cols": [
-            {"id": "", "label": "Topping", "pattern": "", "type": "string"},
-            {"id": "", "label": "Slices", "pattern": "", "type": "number"}
+        'cols': [
+            {'id': '', 'label': 'Topping', 'pattern': '', 'type': 'string'},
+            {'id': '', 'label': 'Slices', 'pattern': '', 'type': 'number'}
         ],
-        "rows": [
-            {"c": [{"v": "Mushrooms", "f": None}, {"v": 3, "f": None}]},
-            {"c": [{"v": "Onions", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Olives", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Zucchini", "f": None}, {"v": 1, "f": None}]},
-            {"c": [{"v": "Pepperoni", "f": None}, {"v": 2, "f": None}]}
+        'rows': [
+            {'c': [{'v': 'Mushrooms', 'f': None}, {'v': 3, 'f': None}]},
+            {'c': [{'v': 'Onions', 'f': None}, {'v': 1, 'f': None}]},
+            {'c': [{'v': 'Olives', 'f': None}, {'v': 1, 'f': None}]},
+            {'c': [{'v': 'Zucchini', 'f': None}, {'v': 1, 'f': None}]},
+            {'c': [{'v': 'Pepperoni', 'f': None}, {'v': 2, 'f': None}]}
         ]
     }
 
     return jsonify(response)
+
+
+@app.route('/bar')
+def bar():
+    return render_template('bar.html')
+
+
+@app.route('/bar_data')
+def bar_data():
+    response = {
+        'cols': [
+            {'id': '', 'label': 'Galaxy', 'type': 'string'},
+            {'id': '', 'label': 'Distance', 'type': 'number'}
+        ],
+        'rows': [
+            {'c': [{'v': 'Canis Major Dwarf'}, {'v': 8000}]},
+            {'c': [{'v': 'Sagittarius Dwarf'}, {'v': 24000}]},
+            {'c': [{'v': 'Ursa Major II Dwarf'}, {'v': 30000}]},
+            {'c': [{'v': 'Lg. Magellanic Cloud'}, {'v': 50000}]},
+            {'c': [{'v': 'Bootes I'}, {'v': 60000}]},
+        ]
+    }
+
+    return jsonify(response)
+
+
+@app.route('/col')
+def col():
+    return render_template('col.html')
+
+
+@app.route('/col_data')
+def col_data():
+    response = {
+        'cols': [
+            {'id': '', 'label': 'Galaxy', 'type': 'string'},
+            {'id': '', 'label': 'Distance', 'type': 'number'},
+            {'id': '', 'label': 'Brightness', 'type': 'number'}
+        ],
+        'rows': [
+            {'c': [{'v': 'Canis Major Dwarf'}, {'v': 8000}, {'v': 23.3}]},
+            {'c': [{'v': 'Sagittarius Dwarf'}, {'v': 24000}, {'v': 4.5}]},
+            {'c': [{'v': 'Ursa Major II Dwarf'}, {'v': 30000}, {'v': 14.3}]},
+            {'c': [{'v': 'Lg. Magellanic Cloud'}, {'v': 50000}, {'v': 0.9}]},
+            {'c': [{'v': 'Bootes I'}, {'v': 60000}, {'v': 13.1}]}
+        ]
+    }
+
+    return jsonify(response)
+
+
+@app.route('/scatter')
+def scatter():
+    return render_template('scatter.html')
