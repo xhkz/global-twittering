7caabc8deab71c07a3c28a538be38ba728953c89
pOOOOOr <au.huangxin@gmail.com>
2015-05-16 23:38:38 +1000

    update requirements and clean codes

--- sentiment/SentiTag.py
+++ sentiment/SentiTag.py
@@ -1,11 +1,10 @@
 __author__ = 'rongzuoliu'
 
 import couchdb
-
 from textblob.en.sentiments import NaiveBayesAnalyzer
 from textblob import TextBlob
 
-from sentiment.TextParser import *
+from TextParser import *
 
 
 def main():
@@ -13,56 +12,40 @@ def main():
     viewname = 'HashtagsView/Hashtags'
 
     server = couchdb.Server(url="http://115.146.95.53:5984/")
-    # server = couchdb.Server()
-    # dbname = 'liberal_followers'
-    # viewname = 'LaborView/Labor'
-
     db = server[dbname]
 
-    # TextParser.getStopWords()
-    # textParser = TextParser()
-
     for row in db.view(viewname):
-        # print row.value
         id = row.id
         doc = db.get(id)
         text = row.value['what']['text']
         print text
-        # parsed_text = textParser.parsing(text)
-        # (sentiment, senti_score) = sentiAnalysisWithTextBlob(text, "NaiveBayesAnalyzer")
         (sentiment, senti_score) = sentiAnalysisWithTextBlob(text, "PatternAnalyzer")
         doc['sentiment'] = {"sentiment": sentiment, "sentiScore": senti_score}
-        # # print doc
         db.save(doc)
 
 
 def sentiAnalysisWithTextBlob(string, classifier):
     sentiment = ''
     senti_score = -100
-    if (classifier == "NaiveBayesAnalyzer"):
+    if classifier == "NaiveBayesAnalyzer":
         blob = TextBlob(string, analyzer=NaiveBayesAnalyzer())
-        # pos/neg is rang from 0-1, the number is bigger, the feeling is stronger
         senti_score = blob.sentiment.p_pos
-        if (senti_score == 0.5):
+        if senti_score == 0.5:
             sentiment = 'neu'
         else:
             sentiment = blob.sentiment.classification
 
-
-    elif (classifier == "PatternAnalyzer"):
+    elif classifier == "PatternAnalyzer":
         blob = TextBlob(string)
         senti_score = blob.sentiment.polarity
-        if (blob.sentiment.polarity > 0):
+        if blob.sentiment.polarity > 0:
             sentiment = 'pos'
-        elif (blob.sentiment.polarity == 0):
+        elif blob.sentiment.polarity == 0:
             sentiment = 'neu'
-        elif (blob.sentiment.polarity < 0):
+        elif blob.sentiment.polarity < 0:
             sentiment = 'neg'
 
-    # print sentiment
-    # print senti_score
-
-    return (sentiment, senti_score)
+    return sentiment, senti_score
 
 
 if __name__ == '__main__':
--- sentiment/TextParser.py
+++ sentiment/TextParser.py
@@ -12,7 +12,6 @@ class TextParser:
     HashTag = []
 
     @staticmethod
-    # get stop word list
     def getStopWords():
         # read the stopwords file and build a list
         TextParser.stopWords = []
@@ -28,60 +27,51 @@ class TextParser:
             line = fp.readline()
         fp.close()
 
-
-    # def __init__(self):
-    # TextParser.getStopWords() # in case
-
     def parsing(self, text):
         text = self.replaceTags(text)
-        # text = self.delete_repeat_characters(text)
         feature_vector = self.getFeatureVector(text)
         parsed_text = ' '.join(feature_vector)
         return parsed_text
 
-
     # replace tags such as mentioned tweeters(@), hashtags(#), URL
     def replaceTags(self, text):
-        #Convert to lower case
+        # Convert to lower case
         text = text.lower()
-        #Convert www.* or https?://* to URL
+        # Convert www.* or https?://* to URL
         text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text)
-        #Convert @username to AT_USER
+        # Convert @username to AT_USER
         self.AtUser = re.findall('@[^\s]+', text)
         text = re.sub('@[^\s]+', 'AT_USER', text)
-        #Replace hashtag #word with the word
-        # tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        # Replace hashtag #word with the word
         self.HashTag = re.findall(r'#[^\s]+', text)
         text = re.sub(r'#([^\s]+)', 'HASH_TAG', text)
-        #Remove additional white spaces
+        # Remove additional white spaces
         text = re.sub('[\s]+', ' ', text)
-        #trim
+        # trim
         text = text.strip('\'"')
         return text
 
-
     # delete the repeated characters in a string. e.g. Baddddddddd! -> Bad!
     def deleteRepeatCharacters(self, text):
-        #look for 2 or more repetitions of character and replace with the character itself
+        # look for 2 or more repetitions of character and replace with the character itself
         pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
         return pattern.sub(r"\1\1", text)
-
 
     # parse tweet to a feature vector
     def getFeatureVector(self, text):
         feature_vector = []
-        #split tweet into words
+        # split tweet into words
         words = text.split()
 
         for w in words:
-            #replace two or more with two occurrences
+            # replace two or more with two occurrences
             w = self.deleteRepeatCharacters(w)
-            #strip punctuation
+            # strip punctuation
             w = w.strip('\'"?,.')
-            #check if the word stats with an alphabet
+            # check if the word stats with an alphabet
             val = re.search(r"^[a-zA-Z][a-zA-Z0-9]*$", w)
-            #ignore if it is a stop word
-            if (w in TextParser.stopWords or val is None):
+            # ignore if it is a stop word
+            if w in TextParser.stopWords or val is None:
                 continue
             else:
                 feature_vector.append(w.lower())
@@ -103,7 +93,3 @@ if __name__ == '__main__':
     # main()
     print 'error!!!'
     print 'The \'TextParser.py\' is a class, which shouldn\'t be called alone!'
-
-
-
-
